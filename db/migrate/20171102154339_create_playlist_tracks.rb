f0c9fe5fef0f2bcc15cd8f830ff630e82ff28873
ripleyofvenus <ripleyofvenus@gmail.com>
2017-11-02 16:13:51 -0400

    Re-Ran Migration During debugging

--- db/migrate/20171102154339_create_playlist_tracks.rb
+++ db/migrate/20171102154339_create_playlist_tracks.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreatePlaylistTracks < ActiveRecord::Migration[5.1]
   def change
     create_table :playlist_tracks do |t|
-      t.references :playlist, foreign_key: true
-      t.references :track, foreign_key: true
+      t.references :playlist, index: true, foreign_key: true
+      t.references :track, index: true, foreign_key: true
 
       t.timestamps
     end
