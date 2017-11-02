0aa7718481030df2dbcb5bcc31e4b7a2dd977a19
ripleyofvenus <ripleyofvenus@gmail.com>
2017-11-02 10:56:45 -0400

    CRUD Actions complete for Playlist
    Tested and confirmed Show:index & one, Create, Update, Delete all
    functional

---
+++ scripts/Playlists/update-playlist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/playlists/${ID}"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+	"playlist": {
+		"name": "'"${NEWNAME}"'"
+	}
+}'
+
+echo
