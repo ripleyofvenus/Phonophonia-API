f912e03a50aae24918ba3c195fdea38954a60b56
ripleyofvenus <ripleyofvenus@gmail.com>
2017-11-03 14:34:21 -0400

    Add line to playlist model for nested attributes
    Added accepts_nested_attributes_for relationship with tracks

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,4 +5,5 @@ class Playlist < ApplicationRecord
   has_many :playlist_tracks, dependent: :destroy
   has_many :tracks, through: :playlist_tracks
   validates :name, :user, presence: true
+  accepts_nested_attributes_for :tracks, allow_destroy: true, update_only: true
 end
