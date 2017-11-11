56753bd3159ce8a8bf627aeb100b51b5ad712aac
ripleyofvenus <ripleyofvenus@gmail.com>
2017-11-11 10:02:29 -0800

    Playlist editable added
    In playlist serializer added and defined editable

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id, :tracks
+  attributes :id, :name, :user_id, :tracks, :editable
+
+  def editable
+    scope == object.user
+  end
 end
