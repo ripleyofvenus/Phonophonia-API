13301b60b7a6a46727a58131c8bc79ddb2608331
ripleyofvenus <ripleyofvenus@gmail.com>
2017-11-12 12:02:48 -0800

    Serializer fix, removing relationship
    Removed false relationship data, replaced with attributes

--- app/serializers/playlist_track_serializer.rb
+++ app/serializers/playlist_track_serializer.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class PlaylistTrackSerializer < ActiveModel::Serializer
-  attributes :id
-  belongs_to :playlist
-  belongs_to :track
+  attributes :id, :playlist_id, :track_id
 end
